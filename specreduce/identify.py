09d81ccc7445a36bd1db991d7223d655f424d3ba
James Davenport <jradavenport@gmail.com>
2020-04-07 07:59:09 -0700

    some identify modes working, interac borked

--- specreduce/identify.py
+++ specreduce/identify.py
@@ -1,12 +1,12 @@
 import ipywidgets as widgets
 import numpy as np
 import matplotlib.pyplot as plt
-from IPython.display import display, clear_output
+from IPython.display import display
 from scipy.optimize import curve_fit
 from scipy.interpolate import UnivariateSpline
 from astropy.table import Table
 
-__all__ = ['identify']
+__all__ = ['identify', 'identify_widget', 'find_peaks']
 
 
 def _gaus(x, a, b, x0, sigma):
@@ -97,10 +97,18 @@ def find_peaks(wave, flux, pwidth=10, pthreshold=97, minsep=1):
     return pcent_pix[okcent], wcent_pix[okcent]
 
 
-def identify(xpixels, flux, identify_mode='interact', fit_mode='spline',
-             linewave=[], autotol=10, polydeg=7, previous_file=''):
+def identify(xpixels, flux, identify_mode='',
+             previous_file='',
+             xpoints=[], wpoints=[],
+             linewave=[], autotol=25,
+             ref_wave=[], ref_flux=[], cbins=50,
+             fit_mode='spline', polydeg=7,):
     '''
     identify's job is to find peaks/features and identify which wavelength they are
+
+    identify_mode: available choices are
+    {'nearest', 'file', 'lines', 'crosscor'}.
+    For an interactive mode, use
 
     identify methods to develop:
     - interactive widget (basically done)
@@ -116,83 +124,15 @@ def identify(xpixels, flux, identify_mode='interact', fit_mode='spline',
 
     '''
 
-    #######
-    if identify_mode.lower() == 'interact':
-        # Keaton Bell helped greatly in getting this simple widget framework put together!
-        # Instructions
-        # ------------
-        # 0) For proper interactive widgets, ensure you're using the Notebook backend
-        # in the Jupyter notebook:
-        # >>> %matplotlib notebook
-        # 1) Click on arc-line features (peaks) in the plot. The Pixel Value box should update.
-        # 2) Enter the known wavelength of the feature in the Wavelength box.
-        # 3) Click the Assign button, a red line will be drawn marking the feature.
-        # 4) Stop the interaction of (or close) the figure.
-        xpxl = []
-        waves = []
-
-        # Create widgets, two text boxes and a button
-        xval = widgets.BoundedFloatText(
-            value=5555.0,
-            min=np.nanmin(xpixels),
-            max=np.nanmax(xpixels),
-            step=0.1,
-            description='Pixel Value (from click):',
-            style={'description_width': 'initial'})
-
-        linename = widgets.Text(  # value='Enter Wavelength',
-            placeholder='Enter Wavelength',
-            description='Wavelength:',
-            style={'description_width': 'initial'})
-
-        button = widgets.Button(description='Assign')
-
-        fig, ax = plt.subplots(figsize=(9, 3))
-
-        # Handle plot clicks
-        def onplotclick(event):
-            # try to fit a Gaussian in the REGION (rgn) near the click
-            rgn = np.where((np.abs(xpixels - event.xdata ) <= 5.))[0]
-            try:
-                sig_guess = 3.
-                p0 = [np.nanmax(flux[rgn]), np.nanmedian(flux), event.xdata, sig_guess]
-                popt, _ = curve_fit(_gaus, xpixels[rgn], xpixels[rgn], p0=p0)
-                # Record x value of click in text box
-                xval.value = popt[2]
-            except RuntimeError:
-                # fall back to click itself if that doesnt work
-                xval.value = event.xdata
-            return
-
-        fig.canvas.mpl_connect('button_press_event', onplotclick)
-
-        # Handle button clicks
-        def onbuttonclick(_):
-            xpxl.append(xval.value)
-            waves.append(linename.value)
-
-            ax.axvline(xval.value, lw=1, c='r', alpha=0.7)
-            return
-
-        button.on_click(onbuttonclick)
-
-        # Do the plot
-        ax.plot(xpixels, flux)
-        plt.draw()
-
-        # Display widgets
-        display(widgets.HBox([xval, linename, button]))
-
-        xpoints = np.array(xpxl)
-        lpoints = np.array(waves)
-
-        # NEED TO DO:
-        # Write the lines to a file for later use (Optional?)
+    # Check that identify mode is valid
 
     #######
     if identify_mode.lower() == 'nearest':
-        # linewave = np.genfromtxt(os.path.join(linelists_dir, linelist), dtype='float',
-        #                          skip_header=1,usecols=(0,),unpack=True)
+        if len(linewave)<1:
+            msg_fail = '''
+            linewave not provided! 
+            For identify_mode='nearest', linewave=[] must be an array of known line wavelengths.'''
+            raise ValueError(msg_fail)
 
         # in this mode, the xpixel input array is actually the approximate
         # wavelength solution (e.g. from the header info)
@@ -207,7 +147,7 @@ def identify(xpixels, flux, identify_mode='interact', fit_mode='spline',
         # this both identifies lines, and has byproduct of ending w/ a spline model
 
         xpoints = np.array([], dtype=np.float) # pixel line centers
-        lpoints = np.array([], dtype=np.float) # wavelength line centers
+        wpoints = np.array([], dtype=np.float) # wavelength line centers
 
         # find center-most lines, sort by dist from center pixels
         ss = np.argsort(np.abs(wcent_pix - np.nanmedian(xpixels)))
@@ -220,40 +160,161 @@ def identify(xpixels, flux, identify_mode='interact', fit_mode='spline',
             if (np.nanmin(np.abs(wcent_guess[ss][i] - linewave)) < autotol):
                 # add corresponding pixel and known wavelength to output vectors
                 xpoints = np.append(xpoints, pcent_pix[ss[i]])
-                lpoints = np.append(lpoints, linewave[np.nanargmin(np.abs(wcent_guess[ss[i]] - linewave))])
+                wpoints = np.append(wpoints, linewave[np.nanargmin(np.abs(wcent_guess[ss[i]] - linewave))])
 
                 # start guessing new wavelength model after first few lines identified
-                if (len(lpoints) > 4):
+                if (len(wpoints) > 4):
                     xps = np.argsort(xpoints)
-                    spl = UnivariateSpline(xpoints[xps], lpoints[xps], ext=0, k=3, s=1e3)
+                    spl = UnivariateSpline(xpoints[xps], wpoints[xps], ext=0, k=3, s=1e3)
                     wcent_guess = spl(pcent_pix)
+        print("Mode='nearest': " + str(len(wpoints)) + ' lines matched.')
+
 
     #######
-    if identify_mode.lower() == "previous":
+    if identify_mode.lower() == "file":
         # read a previously saved .lines file
         if len(previous_file)==0:
             raise ValueError('For identify_mode="previous", `previous_file` must give the path to the previously saved lines file.')
 
         tbl = Table.read(previous_file, format='ascii', names=('pix', 'wave'))
-        xpoints, lpoints = tbl['pix'], tbl['wave']
+        xpoints, wpoints = tbl['pix'], tbl['wave']
+        print("Mode='file': " + str(len(wpoints)) + ' lines used from '+previous_file)
+
+    if identify_mode.lower() == 'lines':
+        if (len(xpoints) != len(wpoints)) | (len(xpoints) == 0):
+            raise ValueError('xpoints and wpoints must match in length, and be greater than 0 length')
+        print("Mode='lines': " + str(len(wpoints)) + ' lines used.')
 
     #######
     # if identify_mode.lower() == 'crosscor':
 
+    # sort the points, just in case the method (or prev run) returns in weird order
+    srt = np.argsort(xpoints)
+    xpoints = xpoints[srt]
+    wpoints = wpoints[srt]
 
-
-    # now turn the (xpixel, wavelength) points -> wavelength(x)
-    # Require at least... 4 lines to generate a solution?
+    # now turn the identified (xpixel, wavelength) points -> wavelength(x)
+    # Require at least... 4 identified lines to generate a solution?
     if (len(xpoints) > 4):
         if (fit_mode.lower() == 'spline'):
             # assuming there is a flux value for every xpixel of interest
             # and that it starts at pixel = 0
             # apply our final wavelength spline solution to the entire array
-            spl = UnivariateSpline(xpoints, lpoints, ext=0, k=3, s=1e3)
+            spl = UnivariateSpline(xpoints, wpoints, ext=0, k=3, s=1e3)
             wavesolved = spl(np.arange(np.size(flux)))
 
         if (fit_mode.lower() == 'poly'):
-            fit = np.polyfit(xpoints, lpoints, polydeg)
+            fit = np.polyfit(xpoints, wpoints, polydeg)
             wavesolved = np.polyval(fit, np.arange(np.size(flux)))
 
+        if (fit_mode.lower() == 'interp'):
+            wavesolved = np.interp(np.arange(np.size(flux)), xpoints, wpoints)
+    else:
+        raise ValueError('Too few lines identified to derive a solution. len(xpoints)='+str(len(xpoints)))
+
     return wavesolved
+
+
+def identify_widget(xpixels, flux, silent=False):
+    '''
+    Interactive version of the Identify GUI, specifically using ipython widgets.
+
+    Each line is roughly identified by the user, then a Gaussian is fit to
+    determine the precise line center. The reference value for the line is then
+    entered by the user.
+
+    When finished, the output lines should usually be passed in a new Jupter
+    notebook cell to `identify` for determining the wavelength solution:
+    >>>> xpl,wav = identify_widget(wapprox, flux) # doctest: +SKIP
+    >>>> new_wave = identify(wapprox, flux, identify_mode='lines', xpoints=xpl, wpoints=wav) # doctest: +SKIP
+
+    NOTE: Because of the widgets used, this is not well suited for inclusion in
+    pipelines, and instead is ideal for interactive analysis.
+
+    Parameters
+    ----------
+    xpixels : `~numpy.ndarray`
+        Array of the pixel values along wavelength dimension of the trace.
+    flux : `~numpy.ndarray`
+        Array of the flux values along the trace. Must have same size as xpixels.
+    silent : bool
+        Set to True to silence the instruction print out each time. (Default: False)
+
+    Returns
+    -------
+    The pixel locations and wavelengths of the identified lines:
+    pixel, wavelength
+    '''
+
+
+    msg = '''
+    Instructions:
+    ------------
+    0) For proper interactive widgets, ensure you're using the Notebook backend
+    in the Jupyter notebook, e.g.:
+        %matplotlib notebook
+    1) Click on arc-line features (peaks) in the plot. The Pixel Value box should update.
+    2) Enter the known wavelength of the feature in the Wavelength box.
+    3) Click the Assign button, a red line will be drawn marking the feature.
+    4) When you've identified all your lines, stop the interaction for (or close) the figure.'''
+
+    if not silent:
+        print(msg)
+
+    xpxl = []
+    waves = []
+
+    # Create widgets, two text boxes and a button
+    xval = widgets.BoundedFloatText(
+        value=5555.0,
+        min=np.nanmin(xpixels),
+        max=np.nanmax(xpixels),
+        step=0.1,
+        description='Pixel Value (from click):',
+        style={'description_width': 'initial'})
+
+    linename = widgets.Text(  # value='Enter Wavelength',
+        placeholder='Enter Wavelength',
+        description='Wavelength:',
+        style={'description_width': 'initial'})
+
+    button = widgets.Button(description='Assign')
+
+    fig, ax = plt.subplots(figsize=(9, 3))
+
+    # Handle plot clicks
+    def onplotclick(event):
+        # try to fit a Gaussian in the REGION (rgn) near the click
+        rgn = np.where((np.abs(xpixels - event.xdata ) <= 5.))[0]
+        try:
+            sig_guess = 3.
+            p0 = [np.nanmax(flux[rgn]), np.nanmedian(flux), event.xdata, sig_guess]
+            popt, _ = curve_fit(_gaus, xpixels[rgn], flux[rgn], p0=p0)
+            # Record x value of click in text box
+            xval.value = popt[2]
+        except RuntimeError:
+            # fall back to click itself if that doesnt work
+            xval.value = event.xdata
+        return
+
+    fig.canvas.mpl_connect('button_press_event', onplotclick)
+
+    # Handle button clicks
+    def onbuttonclick(_):
+        xpxl.append(xval.value)
+        waves.append(linename.value)
+        print(xpxl, waves)
+
+        ax.axvline(xval.value, lw=1, c='r', alpha=0.7)
+        return xpxl, waves
+
+    button.on_click(onbuttonclick)
+
+    # Do the plot
+    ax.plot(xpixels, flux)
+    plt.draw()
+
+    # Display widgets
+    display(widgets.HBox([xval, linename, button]))
+
+    return np.array(xpxl), np.array(waves)
